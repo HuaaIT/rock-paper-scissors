b8afb35c7c2817a0fd1daaa243b77b394a151b20
HuaaIT <sreanglyhour8@gmail.com>
2023-10-12 09:41:11 +0700

    Make function for capitalize string

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 function getComputerChoice(){
     const random = Math.floor(Math.random() * 3) + 1;
     if(random === 1){
-        console.log(random)
-        return console.log("rock");
+        return "Rock";
     }
     else if(random === 2){
-        console.log(random)
-        return console.log("paper");
+        return "Paper";
     }
     else if (random === 3){
-        console.log(random)
-        return console.log("scissors");
+        return "Scissors";
     }
 }
 console.log(getComputerChoice())
-// function playRound(playerSelection, computerSelection){
-//     return "You Lose! Paper beats Rock";
-// }
-// const playerSelection = "rock"
-// const computerSelection = getComputerChoice()
-// console.log(playRound(playerSelection, computerSelection))
+
+function playRound(playerSelection, computerSelection){
+    console.log(playerSelection);
+    return "You Lose! Paper beats Rock";
+}
+
+function capitalize(str){
+    const lower = str.toLowerCase();
+    return str.charAt(0).toUpperCase() + lower.slice(1);
+}
+
+const playerSelection = capitalize("RoCk")
+const computerSelection = getComputerChoice()
+console.log(playRound(playerSelection, computerSelection))
